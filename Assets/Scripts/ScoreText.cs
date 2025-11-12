c7b0495b106e28ff9c5574ee0a3265ad72aa06f4
nopparut14546-collab <nopparut.14546@gmail.com>
2025-11-12 14:14:10 +0700

    Week8 ทำใหม่

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+
+public class ScoreText : MonoBehaviour
+{
+    public ScoreManager scoreManager;
+    public TextMeshProUGUI scoreText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + scoreManager.currentScore;
+    }
+}
